dc888c1bf1662aa4936cd0e6ea4ad3298d53daa5
eecmanny <eec123manny@gmail.com>
2023-08-09 18:43:41 -0400

    questions

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const generateREADME = ({ project, section, description, installation, usage, license, contributing, test, questions, tableOfContent }) =>
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is your README project title?',
+            },
+            {
+                type: 'input',
+                name: 'section',
+                message: 'What is the README section entitled',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'What is the README description entitled',
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'What is your installation?',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'What is your Usage?',
+            },
+            {
+                type: 'input',
+                name: 'license',
+                message: 'What is your license?',
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'What is your contributing?',
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: 'Enter your tests?',
+            },
+            {
+                type: 'input',
+                name: 'questions',
+                message: 'Enter your questions?',
+            },
+            // {
+            //     message: 'Would you like a table of content?',
+            //// Not right
+            //     type: 'list',
+            //     name: 'tableOfContent',
+            //      input: y/n,
+            // },
+        ])
+        .then((answers) => {
+            const pREADMEPageContent = generateREADME(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+            // TODO: Create a function to write README file
+            // function writeToFile(fileName, data) { }
+
+            fs.writeFile('Profesional_README.md', pREADMEPageContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created README.md!')
+            );
+        });
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// If there is no installation, return an empty string
+function renderInstallationBadge(installation) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// TODO: Create a function that returns the usage link
+// If there is no usage, return an empty string
+function renderUsageLink(usage) {}
+
+// TODO: Create a function that returns the credit section of README
+// If there is no credit, return an empty string
+function renderCreditSection(credit) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
